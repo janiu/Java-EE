b4bc595507097cb51acbb47b32c84ed88f296041
janiu <janiu25@gmail.com>
2015-05-09 21:54:55 +0200

    Create JAVA - pole i przycisk i wyslanie na nastepna strone.java

---
+++ JAVA - pole i przycisk i wyslanie na nastepna strone.java
@@ -0,0 +1,5 @@
+String docType = "<<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">";
+out.print(docType+ "<html> <head> <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">");
+out.print("<input type=\"text\" name=\"pole\"><BR>");																										//pole tekstu
+out.print("<input type=\"submit\" value=\"ok\"> </form> </body> </html>");																	//przycisk wysylajacy
+out.print("<title>Insert title here</title> </head> <body> <form name=\"ff\" method=\"get\" action=strona2>");			//przesłanie tekstu do nastepnej strony
