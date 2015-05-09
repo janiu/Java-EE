21ff29ac60d14c7fb794615f10627439051d4a14
janiu <janiu25@gmail.com>
2015-05-09 21:48:09 +0200

    Create JAVA - struktura servlet.java

---
+++ JAVA - struktura servlet.java
@@ -0,0 +1,9 @@
+@WebServlet("/start")
+public class Start extends HttpServlet {
+ 
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+        {                               }
+ 
+        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+        {                               }
+ 
